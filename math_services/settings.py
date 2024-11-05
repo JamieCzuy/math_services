6675b0498673968f3b9cb9fdaeef2ed080290afe
Jamie Czuy <jamie.czuy@gmail.com>
2024-11-05 09:03:51 -0500

    Comment cleanup

--- math_services/settings.py
+++ math_services/settings.py
@@ -25,7 +25,6 @@ SECRET_KEY = 'django-insecure-m#b^hhhku9jfisbm6c-#su+wonka5$+*=%qa7c!lcjg3tq+nk(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '::1']
 ALLOWED_HOSTS = []
 
 
